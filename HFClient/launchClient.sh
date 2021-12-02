e1d2d16386e6b8f6948915e4799586d433efcdd1
binunalex <51915869+binunalex@users.noreply.github.com>
2021-12-02 12:25:01 +0200

    Update launchClient.sh

--- HFClient/launchClient.sh
+++ HFClient/launchClient.sh
@@ -7,7 +7,7 @@ cp ../pom.xml .
 cp ../src/*.java src/
 mkdir wallets
 mvn clean
-./cp_certs.sh 
+. cp_certs.sh 
 mvn install
 cp -f target/HFClient-0.0.1-SNAPSHOT-jar-with-dependencies.jar HFClient.jar
 cp -f *.pem src
